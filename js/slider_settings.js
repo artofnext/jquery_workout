a2a5973f8a25f88942a4bacad382dc1d1f81caf3
artofnext <novnovalex@gmail.com>
2019-12-06 22:07:05 +0200

    init commit

---
+++ js/slider_settings.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+      // setting-name: setting-value
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: true,
+      dots: true,
+      // centerMode: true,
+      // slidesToShow: 1,
+      variableWidth: true,
+      // vertical: true,
+    });
+  });
